cd12b1634f1c8908e78bab1177c332887468984f
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2020-11-20 12:07:32 +0100

    Add dvc fetch

--- sandbox/load_all_xp_results.sh
+++ sandbox/load_all_xp_results.sh
@@ -3,5 +3,5 @@ git fetch -a
 git for-each-ref \
   --format="mkdir -p sandbox/%(refname:short) && git --work-tree=sandbox/%(refname:short) checkout  %(refname:short) -- out.npy.dvc square_err.metric;" \
   refs/remotes/ | xargs -I{} sh -c '{}'
-
+dvc fetch -R sandbox
 dvc checkout -R sandbox
